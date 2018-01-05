5a0311b62a005c494160270d88f6ecc102ceb7e3
= <leobonillab@gmail.com>
2018-01-05 11:04:57 -0300

    cambiado boton de descarga

--- assets/init_tables.js
+++ assets/init_tables.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     $.extend( true, $.fn.dataTable.defaults, {
 		// dom: 'lBfrtip',
 	        buttons: [
-	           // { "extend": 'excel', "text":'<i class="fa fa-download"></span>',"className": 'btn btn-primary btn-sm' }
-	           'excel'
+	            { "extend": 'excel', "text":'<i class="fa fa-download"></span>',"className": 'btn btn-warning btn-sm' }
+	           //'excel'
 	        ],
 	 dom:"lBfrtip",
 	 lengthChange: true,
