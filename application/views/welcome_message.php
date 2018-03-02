106a3befa05d23bc6ec5a9762709fd17b78edfc1
= <leobonillab@gmail.com>
2018-03-02 15:04:02 -0300

    agregado panel realizar llamada

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -15,13 +15,35 @@
                                 </div>
                                 <div class="col-xs-9 text-right">
                                   <!-- <div class="huge">26</div> -->
-                                    <div>SMS</div>
+                                    <div>Sms</div>
                                 </div>
                             </div>
                         </div>
                         <a href="<?php echo base_url('sms/enviar'); ?>">
                             <div class="panel-footer">
                                 <span class="pull-left">Enviar nuevo SMS</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa  fa-phone   fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                  <!-- <div class="huge">26</div> -->
+                                    <div>Generar Llamada</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="<?php echo base_url('llamadas/nueva'); ?>">
+                            <div class="panel-footer">
+                                <span class="pull-left">Hacer una llamada</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                                 <div class="clearfix"></div>
                             </div>
