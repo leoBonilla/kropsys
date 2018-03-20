af905a5d695fcef5770321e95aa70bd6576f736c
= <leobonillab@gmail.com>
2018-03-20 17:59:51 -0300

    fix

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,27 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+
+$db['cdr'] = array(
+	'dsn'	=> '',
+	'hostname' => '192.168.0.150',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'asteriskcdrdb',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
