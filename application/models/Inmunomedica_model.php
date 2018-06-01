0abf1e328652f055501a4e0ce67f084fcf681181
leonardo bonilla <leobonillab@gmail.com>
2018-06-01 16:35:55 -0400

    fix model error

--- application/models/Inmunomedica_model.php
+++ application/models/Inmunomedica_model.php
@@ -54,7 +54,7 @@ class Inmunomedica_model extends CI_Model {
     }
 
     public function cantidad_mensajes($inicio, $fin){
-          $query = $this->db->query("SELECT sum(case when motivo='agendado' then 0 else 1 end) as 'agendados', sum(case when motivo='recordatorio' then 0 else 1 end) as 'recordatorios', count(*) as total FROM `kropsys_service`.`sms` WHERE Date(fecha_envio) BETWEEN '".$inicio."' and '".$fin."'");
+          $query = $this->db->query("SELECT sum(case when motivo='agendado' then 1 else 0 end) as 'agendados', sum(case when motivo='recordatorio' then 1 else 0 end) as 'recordatorios', count(*) as total FROM `kropsys_service`.`sms` WHERE Date(fecha_envio) BETWEEN '".$inicio."' and '".$fin."'");
         if($query->num_rows() > 0){
           return  $query->row();
         }
