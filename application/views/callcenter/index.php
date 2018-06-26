ccdecb4e6f9461e078af15a5c18b9eaba1b4570b
leonardo bonilla <leobonillab@gmail.com>
2018-06-26 16:24:49 -0400

    vista callcenter

---
+++ application/views/callcenter/index.php
@@ -0,0 +1,22 @@
+<style>
+	iframe {
+    display: block;       /* iframes are inline by default */
+    background: #000;
+    border: none;         /* Reset default border */
+    height: 100vh;        /* Viewport-relative units */
+    width: 100%;
+     overflow:hidden;
+}
+</style>
+<div class="col-md-12">
+	<div class="row"><div class="col-md-12" id="callcenter-wrapper">
+		<iframe src='http://192.168.0.205/index.php?menu=agent_console' ></iframe>
+<!-- 		<div class="btn-group">
+  			<button type="button" class="btn btn-danger" disabled="disabled">Colgar</button>
+  			<button type="button" class="btn btn-warning">Descanso</button>
+  			<button type="button" class="btn btn-default" disabled="disabled">Transferir</button>
+		</div>
+ -->
+	<!--	<button type="button" class="btn btn-default pull-right" disabled="disabled">cerrar sesion</button> -->
+	</div></div>
+</div>
